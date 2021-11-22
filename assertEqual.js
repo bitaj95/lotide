fe6a75ce07cf9a6c0b0e799a99722c45229224ef
Bita <bita.janzadeh@hotmail.com>
2021-11-22 18:01:43 -0500

    made assertion checker

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅Assertion Passed: ", actual , " === ", expected);
+  } else {
+    console.log("🚩Assertion Failed: ", actual , " !== ", expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hey!", "hey!");
+assertEqual(1, 5);
