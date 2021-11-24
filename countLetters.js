7fd2b223233f779f25a6db4db031ed121b935af1
Bita <bita.janzadeh@hotmail.com>
2021-11-24 16:48:25 -0500

    changes to countLetters and countOnly

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// Checking function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let result = [];
+  for (const letter of string) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  };
+  delete result[" "];
+  return result;
+};
+
+//Tests
+const result1 = countLetters("Banana");
+assertEqual(result1["B"],1);
--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// Checking function
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
