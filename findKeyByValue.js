556b699f60537048c7b69ddba3b1bb6ae72e9307
Bita <bita.janzadeh@hotmail.com>
2021-11-24 20:39:34 -0500

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+//Bringing over TEST FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(collection, show) {
+
+  let genres = Object.keys(collection);
+  
+  for (const genre of genres) {
+    if (collection[genre] === show) {
+      return genre;
+    }
+  }
+  
+
+//Another Way To Do This: 
+
+  /* for (let genre in collection) {
+    if (collection[genre] === show) {
+      return genre;
+    }
+  } */
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
