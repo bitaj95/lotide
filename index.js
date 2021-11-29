0cc82274d7fe77cc4679672de41d96de8f27585f
Bita <bita.janzadeh@hotmail.com>
2021-11-29 17:39:24 -0500

    used Object Shorthand in ES6

--- index.js
+++ index.js
@@ -18,20 +18,19 @@ const  without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqObjects: eqObjects,
-  findKey: findKey, 
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without:  without
-
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey, 
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
