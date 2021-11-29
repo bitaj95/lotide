c85371330e7df9790376b7539ca1a56d7bfa0cbc
Bita <bita.janzadeh@hotmail.com>
2021-11-29 14:17:27 -0500

    incorporating mocha and chai -> head

--- head.js
+++ head.js
@@ -1,19 +1,10 @@
 const assertEqual = require('./assertEqual');
-//Brought over assertEqual function
-/* const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
- */
+
 const head = function (array) {
   return array[0];
 }
 
 module.exports = head;
-
 
 
 /* assertEqual(head([5,6,7]), 5);
