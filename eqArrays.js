2e454c99c50c4dd2632a3b79e32f25b535fc4aa5
Bita <bita.janzadeh@hotmail.com>
2021-11-29 01:09:26 -0500

    refactored eqArrays using require

--- eqArrays.js
+++ eqArrays.js
@@ -29,9 +29,10 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
+module.exports = eqArrays;
+//exported to eqArraysTest
 
-//test eqArrays through assertEqual function to make sure it is working properly
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
@@ -40,4 +41,5 @@ assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
 assertEqual(eqArrays([[2, 3], [4], [3,4,5,[3]]], [[2, 3], [4], [3,4,5,[3]]]), true) // => true
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)// => false
-assertEqual(eqArrays([[2, 3, [1,2,3]], [4]], [[2, 3], 4]), false)// => false
+assertEqual(eqArrays([[2, 3, [1,2,3]], [4]], [[2, 3], 4]), false)// => false */
+
